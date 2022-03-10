f500ad0e720a4d676bc21336802727f1d6525005
Shreyas Banagar <shreyas.banagar@zycus.com>
2022-03-10 23:56:17 +0530

    Initial version

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
-from calendar import c
+from datetime import datetime
+import numpy as np
 import pandas as pd
 def get_all_errors(file):
     df = pd.read_csv(file)
+    df['GST Amount'] = pd.to_numeric(df['GST Amount'], errors='coerce')
     for ind in df.index:
         for col in df.columns:
             if col == 'PNR':
                 if len(df[col][ind])!=6:
-                    print(f"Row {ind+1} PNR is wrong.",end =' ')    
+                    print(f"Row {ind+2} Col name {col} - PNR is not equal to 6 characters.")    
             elif col == 'Invoice Number':
                 if len(df[col][ind])>16:
-                    print(f"Row {ind+1} Invoice no. is wrong.",end =' ')
+                    print(f"Row {ind+2} Col name {col} - Invoice no. is greater than 16 characters.")
             elif col == 'GST Amount':
-                if  not isinstance(df[col][ind],int) and not isinstance(df[col][ind],float):
-                    print(f"Row {ind+1} GST Amount is wrong.",end =' ')
-            
-
+                if np.isnan(df[col][ind]):
+                    print(f"Row {ind+2} Col name {col} - GST Amount is not a number.")
+            elif col == 'Invoice date':
+                current_date=datetime.now()
+                given_date=datetime.strptime(df[col][ind], "%d-%b-%y")
+                if given_date > current_date:
+                    print(f"Row {ind+2} Col name {col} - Invoice date is greater than the current date.")
+            elif col == 'Customer GSTIN':
+                if len(df[col][ind])!=15:
+                    print(f"Row {ind+2} Col name {col} - Customer GSTIN is not equal to 15 characters.")  
 
 if __name__=='__main__':
     get_all_errors('csv_files/sample.csv')
